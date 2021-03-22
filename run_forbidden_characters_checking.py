34a29b223404378b8049eeef82e6af10ccff4967
1xdeadman <daniilZakharchenko@gmail.com>
2021-03-23 00:22:00 +1000

    - добавил возможность указывать запрещенные символы через аргументы

--- run_forbidden_characters_checking.py
+++ run_forbidden_characters_checking.py
@@ -18,36 +18,39 @@ def save_data(data):
 @click.option(
     "--fieldnames",
     '-f',
-    default="data/column_names_utf-8",
     help="Путь с именем  до файла с именами колонок",
     required=True
 )
 @click.option(
     "--result_file",
     '-r',
-    default='data/result.csv',
     help="Путь с именем для сохранения файла csv",
     required=True
 )
 @click.option(
     "--columns_file",
     '-c',
-    default='data/check_forbidden_characters.log',
     help="Путь с именем для сохранения подсчитанных по колонкам запретных символов",
     required=True
 )
-def run(fieldnames: str, result_file: str, columns_file: str):
+@click.option(
+    "--forbidden_chars",
+    '-o',
+    help="строка запрещенных символов",
+    required=True
+)
+def run(fieldnames: str, result_file: str, columns_file: str, forbidden_chars: str):
+    forbidden_chars = list(forbidden_chars)
     full_start = time()
     column_names = create_columns.get_short_name(fieldnames)
     with open(result_file, 'r', encoding='utf-8') as file:
         reader = csv.DictReader(file, column_names)
         start = time()
         for index, line in enumerate(reader):
-            if '-' in line.values() or '`' in line.values():
-                for key, value in line.items():
-                    if value in ['-', '`']:
-                        save_data(f"{key}: {value}")
-                        # print(key, value)
+            for key, value in line.items():
+                if value in forbidden_chars:
+                    save_data(f"{key}: {value}")
+                    # print(key, value)
             if index % 50000 == 0:
                 print(f"Считано - {index} строк. Потрачено - {round(time() - start, 4)} sec")
                 start = time()
